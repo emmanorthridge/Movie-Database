181caa9add5e7465513e29cd32113df44b915f54
Emma Northridge <emmanorthridge1@gmail.com>
2021-01-26 17:11:22 +0100

    :rocket: Added search functionality

--- App.js
+++ App.js
@@ -1,11 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState } from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import axios from 'axios';
 
 export default function App() {
+
+  const apiurl =  "http://www.omdbapi.com/?i=tt3896198&apikey=c54d4f89"
+  const [state, setState] = useState({
+    s: "Enter a movie...",
+    results: [],
+    selected: {}
+  })
+
+  const search = () => {
+   axios(apiurl + "&s=" + state.s).then(({ data }) => {
+     let results = data.Search
+     console.log(results)
+     setState(prevState => {
+       return {...prevState, results: results }
+     })
+   })
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Movie DB</Text>
+      <TextInput 
+      style={styles.searchbox}
+      onChangeText={text => setState (prevState => {
+        return {...prevState, s: text}
+      }) }
+      onSubmitEditing={search}
+      value={state.s}/>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +40,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#223343',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    paddingTop: 70,
+    paddingHorizontal: 20
   },
+  title: {
+    color: '#FFF',
+    fontSize: 32,
+    // fontWeight: "700",
+    textAlign: "center",
+    marginBottom: 20
+  },
+  searchbox: {
+    fontSize: 20,
+    // fontWeight: "300",
+    padding: 20,
+    width: "100%",
+    backgroundColor: '#FFF',
+    borderRadius: 8,
+    marginBottom: 40
+  }
 });
