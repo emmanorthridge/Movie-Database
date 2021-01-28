c7ce3d54c8261e2021010082460b9ce2df5fbd5b
Emma Northridge <emmanorthridge1@gmail.com>
2021-01-28 11:51:30 +0100

    :sparkles: Search movies  and details rendering

--- App.js
+++ App.js
@@ -1,38 +1,101 @@
 import { StatusBar } from 'expo-status-bar';
-import React, {useState } from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  ScrollView,
+  Image,
+  TouchableHighlight,
+  Modal,
+} from 'react-native';
 import axios from 'axios';
 
 export default function App() {
-
-  const apiurl =  "http://www.omdbapi.com/?i=tt3896198&apikey=c54d4f89"
+  const apiurl = 'http://www.omdbapi.com/?apikey=c54d4f89';
   const [state, setState] = useState({
-    s: "Enter a movie...",
+    s: 'Enter a movie...',
     results: [],
-    selected: {}
-  })
+    selected: {},
+  });
 
   const search = () => {
-   axios(apiurl + "&s=" + state.s).then(({ data }) => {
-     let results = data.Search
-     console.log(results)
-     setState(prevState => {
-       return {...prevState, results: results }
-     })
-   })
-  }
+    axios(apiurl + '&s=' + state.s).then(({ data }) => {
+      let results = data.Search;
+      console.log(results);
+      setState((prevState) => {
+        return { ...prevState, results: results };
+      });
+    });
+  };
+
+  const openPopup = (id) => {
+    axios(apiurl + '&i=' + id).then(({ data }) => {
+      let result = data;
+      console.log(result);
+      setState((prevState) => {
+        return { ...prevState, selected: result };
+      });
+    });
+  };
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Movie DB</Text>
-      <TextInput 
-      style={styles.searchbox}
-      onChangeText={text => setState (prevState => {
-        return {...prevState, s: text}
-      }) }
-      onSubmitEditing={search}
-      value={state.s}/>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Search a movie</Text>
+      <TextInput
+        style={styles.searchbox}
+        onChangeText={(text) =>
+          setState((prevState) => {
+            return { ...prevState, s: text };
+          })
+        }
+        onSubmitEditing={search}
+        value={state.s}
+      />
+
+      <ScrollView style={styles.results}>
+        {state.results.map((result) => (
+          <TouchableHighlight
+            key={result.imdbID}
+            onPress={() => openPopup(result.imdbID)}
+          >
+            <View key={result.imdbID} style={styles.result}>
+              <Image
+                source={{ uri: result.Poster }}
+                style={{
+                  width: '100%',
+                  height: 300,
+                }}
+                resizeMode='cover'
+              />
+              <Text style={styles.heading}>{result.Title}</Text>
+            </View>
+          </TouchableHighlight>
+        ))}
+      </ScrollView>
+      <Modal
+        animationType='fade'
+        transparent={false}
+        visible={typeof state.selected.Title != 'undefined'}
+      >
+        <View style={styles.popup}>
+          <Text style={styles.poptitle}>{state.selected.Title}</Text>
+          <Text style={{ marginBottom: 20 }}>
+            Rating: {state.selected.imdbRating}
+          </Text>
+          <Text>{state.selected.Plot}</Text>
+        </View>
+        <TouchableHighlight
+          onPress={() =>
+            setState((prevState) => {
+              return { ...prevState, selected: {} };
+            })
+          }
+        >
+          <Text style={styles.closeBtn}>Close</Text>
+        </TouchableHighlight>
+      </Modal>
     </View>
   );
 }
@@ -40,26 +103,52 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#223343',
+    backgroundColor: '#B0E0E6',
     alignItems: 'center',
     justifyContent: 'flex-start',
     paddingTop: 70,
-    paddingHorizontal: 20
+    paddingHorizontal: 20,
   },
   title: {
     color: '#FFF',
     fontSize: 32,
-    // fontWeight: "700",
-    textAlign: "center",
-    marginBottom: 20
+    textAlign: 'center',
+    marginBottom: 20,
   },
   searchbox: {
     fontSize: 20,
-    // fontWeight: "300",
     padding: 20,
-    width: "100%",
+    width: '100%',
     backgroundColor: '#FFF',
     borderRadius: 8,
-    marginBottom: 40
-  }
+    marginBottom: 40,
+  },
+  results: {
+    flex: 1,
+  },
+  result: {
+    flex: 1,
+    width: '100%',
+    marginBottom: 20,
+  },
+  heading: {
+    color: '#FFF',
+    fontSize: 18,
+    padding: 20,
+    backgroundColor: '#445565',
+  },
+  popup: {
+    padding: 20,
+  },
+  poptitle: {
+    fontSize: 25,
+    fontWeight: '700',
+    marginBottom: 5,
+  },
+  closeBtn: {
+    padding: 20,
+    fontSize: 20,
+    fontWeight: '700',
+    backgroundColor: '#2484C4',
+  },
 });
